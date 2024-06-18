eb6fd2de0aad9c9941afb2de2065c47e7e28c5cf
ArDmg <40062975+arturrar@users.noreply.github.com>
2024-06-18 12:34:08 +1000

    Update lb1.go

--- lb1.go
+++ lb1.go
@@ -10,7 +10,7 @@ import (
 func main() {
     var name string
     fmt.Println("Введите имя файла содержащего 1 матрицу:")
-    fmt.Scanf("%s\n", &file)
+    fmt.Scanf("%s\n", &txt)
 
     fileName := name + ".txt"
     file, err := os.Open(fileName)
